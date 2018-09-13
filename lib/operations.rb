3c5ac6b537e57bbf522db2d24c6e12925e475df2
Scott M <gmat191@gmail.com>
2018-09-13 04:23:12 +0000

    automatically backed up by learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -7,11 +7,10 @@ def unsafe?(speed)
      false
 end
 end
-
+#evaluated using only if/else statements
 
 def not_safe?(speed)
 	speed > 60 || speed < 40 ? true: false
 end
-	
-
+#took me multiple attempts, first with two seperate lines of speed(> and <) it passed a test of < but not >. By using the double pipe (||) or opeartor I was able to do it in one line. translates directly to if the speed is greater than 60 or the speed is less than 40 you aren't traveling at a safe speed.
 
